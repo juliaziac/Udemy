c63a4129c25e6fdb1ea4abdd1e2988bff861fbec
Julia Ziac <ziac@amyris.com>
2022-03-03 16:57:19 -0800

    Create snake object

--- day_020_snake_game/snake.py
+++ day_020_snake_game/snake.py
@@ -1,12 +1,4 @@
-from turtle import Screen, Turtle
-import time
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
-    
+from turtle import Turtle
     
 class Snake():
     
@@ -14,25 +6,20 @@ class Snake():
         self.starting_positions = [(0,0), (-20,0), (-40,0)]
         self.segments = []
     
-    for position in self.starting_positions:
-        t = Turtle()
-        t.color("white")
-        t.shape("square")
-        t.penup()
-        t.setpos(position)
-        self.segments.append(t)
+        for position in self.starting_positions:
+            t = Turtle()
+            t.color("white")
+            t.shape("square")
+            t.penup()
+            t.setpos(position)
+            self.segments.append(t)
     
     
-    def __move__(self):
-        game_is_on = True
-
-        while game_is_on:
-            screen.update()
-            time.sleep(1)
-            for seg_num in range(len(self.segments)-1, 0, -1):
-                new_x = self.segments[seg_num-1].xcor()
-                new_y = self.segments[seg_num-1].ycor()
-                self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(20)
+    def move(self):
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[seg_num-1].xcor()
+            new_y = self.segments[seg_num-1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
         
     
